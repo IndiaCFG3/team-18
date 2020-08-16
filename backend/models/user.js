80393802113b55f4b64f772b589a1c3aaad10014
softhax <aashu1999@gmail.com>
2020-08-16 11:03:19 +0530

    model:added user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,1 +1,25 @@
 // We will define our user collection here.
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 10,
+  },
+  isAdmin: Boolean,
+  isUser: Boolean,
+});
